ca1d70e10038c70afef7b361151d082f53fd328d
XantX <hp_xantx@hotmail.com>
2024-07-30 03:04:23 -0500

    Add UserDto

--- src/main/java/com/example/rest_service/user/infraestructure/controller/UserController.java
+++ src/main/java/com/example/rest_service/user/infraestructure/controller/UserController.java
@@ -2,10 +2,11 @@ package com.example.rest_service.user.infraestructure.controller;
 
 import com.example.rest_service.user.application.usercases.UserUseCase;
 import com.example.rest_service.user.domain.entities.User;
+import com.example.rest_service.user.infraestructure.controller.dtos.UserDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,7 +18,8 @@ public class UserController {
     }
 
     @GetMapping("/users")
-    public List<User> getUsers() {
-        return userUseCase.getUser(2);
+    public List<UserDto> getUsers(@RequestParam(name = "limit", required = false, defaultValue = "10") int limit) {
+        List<User> users = userUseCase.getUser(limit);
+        return users.stream().map(user -> new UserDto(user.getName(), user.getLastname())).toList();
     }
 }
